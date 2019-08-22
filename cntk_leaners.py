00d0944a7a1e4fdd5f45129fcb39dfd9028a7126
윤두밈(Dumim Yoon) <yoondm@ncsoft.mdev>
2019-08-22 19:07:57 +0900

    RAdam 버그 수정.

--- cntk_leaners.py
+++ cntk_leaners.py
@@ -17,18 +17,17 @@ class MySgd(UserLearner):
 # https://github.com/LiyuanLucasLiu/RAdam/blob/master/radam.py
 
 class RAdam(UserLearner):
-    def __init__(self, parameters, lr_schedule, beta1: float, beta2: float, weight_decay: float = 0):
+    def __init__(self, parameters, lr_schedule, beta1: float = 0.9, beta2: float = 0.999, weight_decay: float = 0, eps: float = 1e-12):
         super(RAdam, self).__init__(parameters, lr_schedule)
         self.state = {}
         self.betas = (C.Constant(beta1), C.Constant(beta2))
         self.buffer = [[None, None, None] for ind in range(10)]
-        self.eps = C.Constant(1e-12)
+        self.eps = C.Constant(eps)
+        self.weight_decay = weight_decay
 
     def update(self, gradient_values, training_sample_count, sweep_end):
         lr = self.learning_rate()
         for p, g in gradient_values.items():
-            grad = C.Constant(g)
-            p = p
 
             if p not in self.state:
                 self.state[p] = {}
@@ -36,18 +35,16 @@ class RAdam(UserLearner):
             state = self.state[p]
 
             if len(state) == 0:
+                print('new state')
                 state['step'] = 0
                 state['exp_avg'] = C.Constant(C.zeros_like(p).eval())
                 state['exp_avg_sq'] = C.Constant(C.zeros_like(p).eval())
-            else:
-                state['exp_avg'] = state['exp_avg'] # .type_as(p)
-                state['exp_avg_sq'] = state['exp_avg_sq'] # .type_as(p)
 
             exp_avg,  exp_avg_sq = state['exp_avg'],  state['exp_avg_sq']
             beta1, beta2 = self.betas
 
-            exp_avg_sq.set_value( (exp_avg_sq*beta2 + (1-beta2)*grad*grad).eval(as_numpy=False).data )
-            exp_avg.set_value( (exp_avg*beta1 + ((1-beta1)+grad)).eval(as_numpy=False).data )
+            exp_avg_sq.set_value( (exp_avg_sq*beta2 + (1-beta2) * g*g).eval(as_numpy=False).data )
+            exp_avg.set_value   ( (exp_avg*beta1    + (1-beta1) * g  ).eval(as_numpy=False).data )
 
             state['step'] += 1
             buffered = self.buffer[int(state['step']%10)]
@@ -66,14 +63,16 @@ class RAdam(UserLearner):
                     step_size = lr / (1 - C.pow(beta1, state['step']))
                 buffered[2] = step_size.eval()
 
-            # if group['weight_decay'] != 0:
-            #     p.add_(-group['weight_decay'] * group['lr'], p)
+            new_p = p
+
+            if self.weight_decay != 0:
+                new_p += (-self.weight_decay * lr * p)
 
             if N_sma >= 5:
                 denom = C.sqrt(exp_avg_sq) + self.eps
-                new_p = p + (-step_size*(exp_avg/denom))
+                new_p += (-step_size * (exp_avg/denom))
             else:
-                new_p = p + (-step_size + exp_avg)
+                new_p += (-step_size * (exp_avg))
             
             p.set_value(new_p.eval(as_numpy=False).data)
         return True
@@ -92,7 +91,9 @@ loss = C.reduce_mean(C.square(b))
 
 # trainer = C.Trainer(b, (loss, None), C.universal(my_adagrad, b.parameters))
 # trainer = C.Trainer(b, (loss, None), MySgd(b.parameters, C.learning_parameter_schedule(0.01)))
-trainer = C.Trainer(b, (loss, None), RAdam(b.parameters, C.learning_parameter_schedule(0.01), 0.1, 0.9))
+trainer = C.Trainer(b, (loss, None), RAdam(b.parameters, C.learning_parameter_schedule(0.01))) #, 0.9, 0.999))
 # trainer = C.Trainer(b, (loss, None), C.sgd(b.parameters, C.learning_parameter_schedule(0.01)))
 
-trainer.train_minibatch({a:np.array([[1.0]])})
+count = 100
+for i in range(count):
+    print(f'i({i}/{count}):{trainer.train_minibatch({a:np.array([[1.0]])}, outputs=[loss])[1][loss]}')
